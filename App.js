bbe713776756d477d695230c2d20a8f0dbc78f7c
kim-ly <SoSe2018>
2019-09-17 16:34:49 +0200

    Includes Searchbar

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
-
+import { SearchBar } from 'react-native-elements';
 import Recipe from './src/Recipe';
 
 export default class App extends React.Component {
+  state = {
+    search: '',
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
   render() {
+    const { search } = this.state;
+
     return (
       <View style={{
           flexDirection: 'row',
           padding: 20,
         }}>
+          <SearchBar
+          placeholder="Type Here..."
+          onChangeText={this.updateSearch}
+          value={search}
+        />
         <Recipe />
       </View>
     );
---
+++ src/components/SearchBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { SearchBar } from 'react-native-elements';
+
+const Searchbar = props => {
+
+};
+
+export default SearchBar;
